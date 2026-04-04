766a74b2d3deef472ec162242d4a27a9d6a51002
Lower1K <jacksonedward@gmail.com>
2026-04-04 17:00:43 -0400

    Add missing variables

--- src/discord.js
+++ src/discord.js
@@ -6,6 +6,9 @@ export async function handleDiscordRequest(request, env) {
 		return new Response("Method not allowed", { status: 405 });
 	}
 
+	const applicationId = env.DISCORD_APPLICATION_ID;
+	const interactionToken = json.token;
+
 	const signature = request.headers.get("x-signature-ed25519");
 	const timestamp = request.headers.get("x-signature-timestamp");
 
