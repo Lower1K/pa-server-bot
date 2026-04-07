883c1982c662c80125a407f94a9f1e7599a9e477
Lower1K <jacksonedward@gmail.com>
2026-04-06 21:16:30 -0400

    Revert "Change image links"
    This reverts commit a30d453599aeca12d25fe4270adb7ed98f938640.

--- src/discord.js
+++ src/discord.js
@@ -95,7 +95,7 @@ export async function handleDiscordRequest(request, env, ctx) {
 				type: 4,
 				data: {
 					// The creature
-					content: "https://drive.google.com/file/d/1ohXPhK99NY6KZg-lAmizGUhUkNS8-8Vk/view?usp=drive_link",
+					content: "https://media.discordapp.net/attachments/1251262347952128116/1450904311205728508/Aatrox_Keanu.png?ex=69cea8f0&is=69cd5770&hm=183093f08c309aec6d474ccd68ae98b9aaf9236e62012280f0ac96436ca7a904&=&format=webp&quality=lossless&width=720&height=960",
 				},
 			});
 		}
@@ -104,7 +104,7 @@ export async function handleDiscordRequest(request, env, ctx) {
 			const response = Response.json({
 				type: 4,
 				data: {
-					content: "https://drive.google.com/file/d/1ckHXaMKNQ9Aul0YNw50eb-XvJla9_QfV/view?usp=drive_link",
+					content: "https://media.discordapp.net/attachments/1347637679126347787/1490184659030773840/Screenshot_2026-02-20_011616.png?ex=69d3221d&is=69d1d09d&hm=0c2c2cb38df85f26a37f5fa7d2ba12abd148490b725658884d4352a4131ab250&=&format=webp&quality=lossless",
 				},
 			});
 
@@ -126,7 +126,7 @@ export async function handleDiscordRequest(request, env, ctx) {
 			return Response.json({
 				type: 4,
 				data: {
-					content: "https://drive.google.com/file/d/1PnfOrqLUFMwzrv9byyX9prLQaE4NBNYH/view?usp=drive_link",
+					content: "https://media.discordapp.net/attachments/1347637679126347787/1490184659298947224/Screenshot_2026-01-02_165915.png?ex=69d3cadd&is=69d2795d&hm=7ec0a1bf9cffce80e52a05d736ff51ed01960f71cc287dd4829046efa2425dd6&=&format=webp&quality=lossless",
 				},
 			});
 		}
@@ -165,7 +165,7 @@ export async function handleDiscordRequest(request, env, ctx) {
 			const response = Response.json({
 				type: 4,
 				data: {
-					content: "https://drive.google.com/file/d/1E-IFN97lYyp1frqu9m6MPAaBDdJn3-OJ/view?usp=drive_link",
+					content: "https://media.discordapp.net/attachments/1487899760756134021/1489398014328967238/gyattrox.gif?ex=69d0457e&is=69cef3fe&hm=ccb1fcc30c564a34323c6618ea9097d39b2be73da4f2050d97e225c7c67973c1&=",
 				},
 			});
 
@@ -188,7 +188,7 @@ export async function handleDiscordRequest(request, env, ctx) {
 				type: 4,
 				data: {
 					// Minion laughing GIF
-					content: "https://drive.google.com/file/d/1oPt4smQVWDRNTvIo0cwCG3-GAtFOtk1z/view?usp=drive_link",
+					content: "https://media.discordapp.net/attachments/1114377224099995650/1489341429531807764/minion.gif?ex=69d010cb&is=69cebf4b&hm=90b95cf16c5782f9c1cc91067b1e453c70f9b14be5e65fa969f372675528ed11&=",
 				},
 			});
 		}
@@ -197,7 +197,7 @@ export async function handleDiscordRequest(request, env, ctx) {
 				type: 4,
 				data: {
 					// Horse middle-finger
-					content: "https://drive.google.com/file/d/1MtbLDI1fOqqljmiV7oYaMNmuNXSyaUoU/view?usp=drive_link",
+					content: "https://media.discordapp.net/attachments/1215052651159625873/1398088581272043540/Bb5Dz_4mUQxtVc10-ezgif.com-video-to-gif-converter.gif?ex=69d24b35&is=69d0f9b5&hm=c0d081a2910aa1c29571de45012c46785863dba9c4bae6a59ca127d4ad00304d&=",
 				},
 			});
 		}
@@ -266,7 +266,7 @@ export async function handleDiscordRequest(request, env, ctx) {
 				type: 4,
 				data: {
 					// Skeleton spin video
-					content: "https://drive.google.com/file/d/10wuLhOL9HEmPyMkYUEOxrE5y0mlOrDXi/view?usp=drive_link",
+					content: "https://cdn.discordapp.com/attachments/1100940491765321801/1490170902783070248/videoplayback.mp4?ex=69d3154d&is=69d1c3cd&hm=ef2960ecc6c2f0cf91a617beffae5f1b23e56bdc6aa84775e4a39319799cfd81&",
 				}
 			})
 		}
